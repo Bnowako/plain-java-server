2b922b08bf0cc60875496008addc80434d86a665
Bnowako <blazejnowakowski98@gmail.com>
2022-07-03 12:39:55 +0200

    create example app that uses Yen Jinn server
    Co-authored-by: moscicky <moscicki.maciej619@gmail.com>

---
+++ example/src/main/java/mono/Main.java
@@ -0,0 +1,11 @@
+package mono;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Jinn sever = new Jinn();
+        sever.makeAWish();
+    }
+}
