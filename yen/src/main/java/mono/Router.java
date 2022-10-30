070b5e453ac0a921017daa049cf65d873db97813
Bnowako <blazejnowakowski98@gmail.com>
2022-10-30 17:57:23 +0100

    Throw 500 if something goes wrong in router

--- yen/src/main/java/mono/Router.java
+++ yen/src/main/java/mono/Router.java
@@ -12,23 +12,26 @@ public class Router {
     private final KlaksonDeser deser = new KlaksonDeser();
 
     public HttpResponse route(HttpRequest httpRequest, Map<String, Controller> endpoints) {
-        List<String> parts = getPathParts(httpRequest.path());
-        Controller controller = endpoints.get(parts.get(1));
-        if(controller == null) throw new IllegalArgumentException("Path does not exist");
+        try {
+            List<String> parts = getPathParts(httpRequest.path());
+            Controller controller = endpoints.get(parts.get(1));
+            if (controller == null) throw new IllegalArgumentException("Path does not exist");
 
-        String secondSegment = parts.size() < 3 ? "" : parts.get(2);
-        Endpoint endpoint = controller.getEndpoint(secondSegment, httpRequest.method());
-        if(endpoint == null) throw new IllegalArgumentException("Path does not exist");
+            String secondSegment = parts.size() < 3 ? "" : parts.get(2);
+            Endpoint endpoint = controller.getEndpoint(secondSegment, httpRequest.method());
+            if (endpoint == null) throw new IllegalArgumentException("Path does not exist");
 
-        Object deserBody = null;
-        if(endpoint.bodyClass() != null) {
-            if(httpRequest.body().isEmpty()) throw new IllegalArgumentException("Body must be present");
-            deserBody = deser.deser(httpRequest.body().get(), endpoint.bodyClass());
-            System.out.printf("Deser done! body: %s%n", deserBody.toString());
+            Object deserBody = null;
+            if (endpoint.bodyClass() != null) {
+                if (httpRequest.body().isEmpty()) throw new IllegalArgumentException("Body must be present");
+                deserBody = deser.deser(httpRequest.body().get(), endpoint.bodyClass());
+                System.out.printf("Deser done! body: %s%n", deserBody.toString());
+            }
+            Object fulfilledWish = endpoint.wish().fulfill(httpRequest, deserBody);
+            return HttpResponseFactory.createResponse(deser.ser(fulfilledWish), HttpStatus.OK);
+        } catch (Throwable e) {
+            return HttpResponseFactory.getErrorResponse();
         }
-
-        Object fulfilledWish = endpoint.wish().fulfill(httpRequest, deserBody);
-        return HttpResponseFactory.createResponse(deser.ser(fulfilledWish), HttpStatus.OK);
     }
 
     private List<String> getPathParts(String path) {
