abee31a945866e16e07e52bb647cec0afac7fded
Bnowako <blazejnowakowski98@gmail.com>
2022-10-30 17:57:09 +0100

    Add 500 status code and error response

--- yen/src/main/java/mono/http/HttpResponseFactory.java
+++ yen/src/main/java/mono/http/HttpResponseFactory.java
@@ -19,6 +19,12 @@ public class HttpResponseFactory {
         return new HttpResponse("HTTP/1.1", HttpStatus.OK, headers, body);
     }
 
+    public static HttpResponse getErrorResponse() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Date", LocalDateTime.now(ZoneOffset.UTC).toString());
+        return new HttpResponse("HTTP/1.1", HttpStatus.SERVER_ERROR, headers);
+    }
+
     public static HttpResponse createResponse(String body, HttpStatus httpStatus) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Date", LocalDateTime.now(ZoneOffset.UTC).toString());
--- yen/src/main/java/mono/http/HttpStatus.java
+++ yen/src/main/java/mono/http/HttpStatus.java
@@ -4,7 +4,8 @@ public enum HttpStatus {
     OK(200),
     BAD_REQUEST(400),
     NOT_FOUND(404),
-    CREATED(201);
+    CREATED(201),
+    SERVER_ERROR(500);
 
     public final int code;
 
